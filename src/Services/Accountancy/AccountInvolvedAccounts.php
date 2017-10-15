c258f3da95bd777b0e50511845f9262dde5c6183
Koen Hoeijmakers <koen@hoeijmakers.me>
2017-10-15 21:53:00 +0200

    Update AccountInvolvedAccounts

--- src/Services/Accountancy/AccountInvolvedAccounts.php
+++ src/Services/Accountancy/AccountInvolvedAccounts.php
@@ -6,5 +6,33 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class AccountInvolvedAccounts extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/AccountInvolvedAccounts';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Account',
+        'AccountName',
+        'Created',
+        'Creator',
+        'CreatorFullName',
+        'Division',
+        'InvolvedAccount',
+        'InvolvedAccountRelationTypeDescription',
+        'InvolvedAccountRelationTypeDescriptionTermId',
+        'InvolvedAccountRelationTypeId',
+        'Modified',
+        'Modifier',
+        'ModifierFullName',
+        'Notes',
+    ];
 }
