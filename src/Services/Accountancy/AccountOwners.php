3f2c2059ee9261352e05f517fb7fb16c1a83726a
Koen Hoeijmakers <koen@hoeijmakers.me>
2017-10-15 21:55:48 +0200

    Update AccountOwners

--- src/Services/Accountancy/AccountOwners.php
+++ src/Services/Accountancy/AccountOwners.php
@@ -6,5 +6,33 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class AccountOwners extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/AccountOwners';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Account',
+        'AccountCode',
+        'AccountName',
+        'Created',
+        'Creator',
+        'CreatorFullName',
+        'Division',
+        'Modified',
+        'Modifier',
+        'ModifierFullName',
+        'OwnerAccount',
+        'OwnerAccountCode',
+        'OwnerAccountName',
+        'Shares',
+    ];
 }
