19b76cbfab65969e0eba02c57e415ee28a8f847d
Koen Hoeijmakers <koen@hoeijmakers.me>
2017-10-15 20:53:54 +0200

    Prepared Bulk services

---
+++ src/Services/Bulk/Documents/Documents.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace KoenHoeijmakers\LaravelExact\Services\Bulk\Documents;
+
+use KoenHoeijmakers\LaravelExact\Services\Service;
+
+class Documents extends Service
+{
+
+}
---
+++ src/Services/Bulk/Financial/TransactionLines.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace KoenHoeijmakers\LaravelExact\Services\Bulk\Financial;
+
+use KoenHoeijmakers\LaravelExact\Services\Service;
+
+class TransactionLines extends Service
+{
+
+}
