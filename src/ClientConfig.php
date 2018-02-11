c89200e8d152cb9a2ac4b9df0eccab1dde3f7414
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-11 21:45:46 +0100

    Setup ClientConfig

---
+++ src/ClientConfig.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace KoenHoeijmakers\LaravelExact;
+
+use Illuminate\Support\Str;
+
+class ClientConfig
+{
+    /**
+     * The client id.
+     *
+     * @var integer
+     */
+    protected $clientId;
+
+    /**
+     * The client secret.
+     *
+     * @var string
+     */
+    protected $clientSecret;
+
+    /**
+     * The access token.
+     *
+     * @var string
+     */
+    protected $accessToken;
+
+    /**
+     * The refresh token.
+     *
+     * @var string
+     */
+    protected $refreshToken;
+
+    /**
+     * The division.
+     *
+     * @var int
+     */
+    protected $division;
+
+    /**
+     * ClientConfig constructor.
+     *
+     * @param array $payload
+     */
+    public function __construct(array $payload = [])
+    {
+        $this->setConfigFromPayload($payload);
+    }
+
+    /**
+     * Set the config by the given payload.
+     *
+     * @param array $payload
+     */
+    protected function setConfigFromPayload(array $payload = [])
+    {
+        foreach ($payload as $key => $value) {
+            if (method_exists($this, $method = $this->getSetterMethodFromKey($key))) {
+                $this->{$method}($value);
+            }
+        }
+    }
+
+    /**
+     * Get the setter method for the given key.
+     *
+     * @param $key
+     * @return string
+     */
+    protected function getSetterMethodFromKey($key)
+    {
+        return 'set' . Str::studly($key);
+    }
+
+    /**
+     * Get the client secret.
+     *
+     * @return mixed
+     */
+    public function getClientSecret()
+    {
+        return $this->clientSecret;
+    }
+
+    /**
+     * Set the client secret.
+     *
+     * @param mixed $clientSecret
+     * @return $this
+     */
+    public function setClientSecret($clientSecret)
+    {
+        $this->clientSecret = $clientSecret;
+
+        return $this;
+    }
+
+    /**
+     * Get the refresh token.
+     *
+     * @return mixed
+     */
+    public function getRefreshToken()
+    {
+        return $this->refreshToken;
+    }
+
+    /**
+     * Set the refresh token.
+     *
+     * @param mixed $refreshToken
+     * @return $this
+     */
+    public function setRefreshToken($refreshToken)
+    {
+        $this->refreshToken = $refreshToken;
+
+        return $this;
+    }
+
+    /**
+     * Get the access token.
+     *
+     * @return mixed
+     */
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
+    /**
+     * Set the access token.
+     *
+     * @param mixed $accessToken
+     * @return $this
+     */
+    public function setAccessToken($accessToken)
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get the client id.
+     *
+     * @return mixed
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * Set the client id.
+     *
+     * @param mixed $clientId
+     * @return $this
+     */
+    public function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
+    /**
+     * Get the division.
+     *
+     * @return mixed
+     */
+    public function getDivision()
+    {
+        return $this->division;
+    }
+
+    /**
+     * Set the division.
+     *
+     * @param mixed $division
+     * @return $this;
+     */
+    public function setDivision($division)
+    {
+        $this->division = $division;
+
+        return $this;
+    }
+}
