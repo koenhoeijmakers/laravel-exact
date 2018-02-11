1c0bd284e0ecf925bd0653586f092a4e175cc1d9
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-11 22:01:36 +0100

    Remove the enabled config item

--- config/exact.php
+++ config/exact.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'enabled'       => env('EXACT_ENABLED', false), // We'll give you the control over the exact.
     'client_id'     => env('EXACT_CLIENT_ID'),      // Your exact client id.
     'client_secret' => env('EXACT_CLIENT_SECRET'),  // Your client secret.
     'division'      => env('EXACT_DIVISION'),       // Your division.
