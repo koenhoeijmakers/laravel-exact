36c44d9c68c11dc00a47e1e0c7167f99afbfe24a
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-26 10:01:15 +0100

    update InvolvedUsers service

--- src/Services/Accountancy/InvolvedUsers.php
+++ src/Services/Accountancy/InvolvedUsers.php
@@ -6,5 +6,42 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class InvolvedUsers extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/InvolvedUsers';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Account',
+        'AccountCity',
+        'AccountCode',
+        'AccountIsSupplier',
+        'AccountLogoThumbnailUrl',
+        'AccountName',
+        'AccountStatus',
+        'Created',
+        'Creator',
+        'CreatorFullName',
+        'Division',
+        'InvolvedUserRole',
+        'InvolvedUserRoleDescription',
+        'IsMainContact',
+        'Modified',
+        'Modifier',
+        'ModifierFullName',
+        'PersonEmail',
+        'PersonPhone',
+        'PersonPhoneExtension',
+        'PersonPictureThumbnailUrl',
+        'User',
+        'UserFullName',
+    ];
 }
