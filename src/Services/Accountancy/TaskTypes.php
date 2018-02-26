897719f2f99992e2e0173258e31e355e451859e0
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-26 10:01:34 +0100

    update TaskTypes service

--- src/Services/Accountancy/TaskTypes.php
+++ src/Services/Accountancy/TaskTypes.php
@@ -6,5 +6,28 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class TaskTypes extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/TaskTypes';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Created',
+        'Creator',
+        'CreatorFullName',
+        'Description',
+        'DescriptionTermID',
+        'Division',
+        'Modified',
+        'Modifier',
+        'ModifierFullName',
+    ];
 }
