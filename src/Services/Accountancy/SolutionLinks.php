d373e5ab84f89ca3df59d200bb16663f2bb98f37
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-26 10:01:28 +0100

    update SolutionLinks service

--- src/Services/Accountancy/SolutionLinks.php
+++ src/Services/Accountancy/SolutionLinks.php
@@ -6,5 +6,33 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class SolutionLinks extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/SolutionLinks';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Account',
+        'Created',
+        'Creator',
+        'Division',
+        'ExternalSolutionCode',
+        'ExternalSolutionName',
+        'ExternalSolutionUrl',
+        'InternalSolutionDivision',
+        'Modified',
+        'Modifier',
+        'Name',
+        'OtherExternalSolutionName',
+        'SolutionType',
+        'Status',
+    ];
 }
