87f1d313e54cc882059459e658a2f29adc660fe4
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-02-26 09:58:35 +0100

    update InvolvedUserRoles service

--- src/Services/Accountancy/InvolvedUserRoles.php
+++ src/Services/Accountancy/InvolvedUserRoles.php
@@ -6,5 +6,29 @@ use KoenHoeijmakers\LaravelExact\Services\Service;
 
 class InvolvedUserRoles extends Service
 {
+    /**
+     * The resource uri.
+     *
+     * @var string
+     */
+    protected $resourceUri = '/api/v1/{division}/accountancy/InvolvedUserRoles';
 
+    /**
+     * The model's fillable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ID',
+        'Code',
+        'Created',
+        'Creator',
+        'CreatorFullName',
+        'Description',
+        'DescriptionTermID',
+        'Division',
+        'Modified',
+        'Modifier',
+        'ModifierFullName',
+    ];
 }
